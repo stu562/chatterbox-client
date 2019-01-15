78f63f155999aa804529f89ba4530f2b259a54e9
stu562 <stu562@gmail.com>
2019-01-14 16:25:14 -0800

    AJAX behavior tests passing

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,19 @@
 var MessageView = {
-
+  //referenced in messagesview 
+  //chat is one message 
+  //we think this is the view - create NODE here -
+  //associated w/ 0 or 1 model objects
+  //inheirent from messages.js
   render: _.template(`
-      <!--
-      <div class="chat">
+      <div id="chats">
         <div class="username"></div>
         <div></div>
       </div>
-      -->
-    `)
+    `),
+  html: this.render(message),//creates the obj
+  // if we add line 19 as a property, 
 
 };
+
+var message = new Messages();//obj
+
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -1,11 +1,19 @@
 var MessagesView = {
 
   $chats: $('#chats'),
-
+  
   initialize: function() {
+    //creating the message
+    var newMessage = new Messages();
+    return newMessage;
   },
-
-  render: function() {
+  
+  
+  renderMessage: function(message) {
+    // var message = initialize();
+    //attach message to DOM - needs to be created for each 
+    $chats.prepend(message);
   }
 
 };
+
